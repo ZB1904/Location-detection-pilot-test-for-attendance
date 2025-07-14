59e496f28c0835c540145be998a7f8a2905fa7e7
ZB1904 <lancebagaoisan64@gmail.com>
2025-07-14 12:37:17 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ const school_latitude=122.011648;
 const school_longitude=18.3042048;
 const minimum_distance_in_meters=100;
 
+
 const user_name=document.getElementById("Name");
 const Id=document.getElementById("ID Number");
 const submit_button=document.getElementById("submit_btn");
 const display=document.getElementById("Text");
+
+
+
 
 
 
@@ -35,12 +39,14 @@ display.style.whiteSpace = 'pre-line';
 
 
 
+
 //Functions
 function submit(x,y)
 {
     submit_button.onclick=function()
     {
-        !user_name.value || !Id.value ? display.textContent="please enter a name and Id" : display.textContent=`Hello! ${user_name.value}\nYour Id is: ${Id.value}\nyour longitude is: ${x}\n and your latitude is: ${y}\nyou are:${'n'}`;
+        !user_name.value || !Id.value ? display.textContent="please enter a name and Id" : display.textContent=`Hello! ${user_name.value}\nYour Id is: ${Id.value}\nyour longitude is: ${student_longitude}\n and your latitude is: ${student_latitude}\nyou are: ${'n'}`;
+        window.prompt(`${convert_degrees_to_radians()}:${convert_degrees_to_radians()}`)
     };
     
 }
@@ -55,9 +61,20 @@ function test_compatibility()
 }
 function Get_coordinates(position)
 {
-    const y=position.coords.latitude;
-    const x=position.coords.longitude;
-    submit(x,y)
+    const student_latitude=position.coords.latitude;
+    const student_longitude=position.coords.longitude;
+    const distance = Get_distance(student_latitude,student_latitude,school_latitude,school_longitude);
+
+    if(distance < minimum_distance_in_meters)
+        {
+            display.textContent=`sorry but you are ${distance-minimum_distance_in_meters} meters outside of the school grounds`
+        }
+    else
+    {
+        display.textContent=`welcome back ${user_name.value} have a pleasant day`
+    }
+    
+    submit(student_longitude,student_latitude)
 
 
     return;
@@ -68,8 +85,22 @@ function Get_Unsuccesful()
     return;
 }
 
-// Lo = longitude La = latitude
-function convert_degrees_to_radians(School_lo,Schoo_la,user_lo,user_la)
-{
 
+function convert_degrees_to_radians(degrees)
+{
+    return degrees*(Math.PI /180);
+}Math
+
+
+function Get_distance(student_latitude,student_longitude,school_latitude,school_longitude)
+{
+    const Earth_radius = 6371000;
+
+    const compare_latitude_distance=convert_degrees_to_radians(student_latitude,school_latitude);
+    const compare_longitude_distance=convert_degrees_to_radians(student_longitude,school_longitude);
+
+    const formulate = Math.sin(compare_latitude_distance/2) **2 +Math.cos(convert_degrees_to_radians(student_latitude))* Math.cos(convert_degrees_to_radians(school_latitude)) * Math.sin(compare_longitude_distance/2) ** 2;
+    const meters = Math.atan2(Math.sqrt(formulate), Math.sqrt(1-formulate));
+
+    return Earth_radius * meters;
 }
